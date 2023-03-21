edc7db7b22d49c94a3cfef02e84a14bbbfcf81d5
Jeromelee97 <jeromeleeking1997@gmail.com>
2023-03-21 21:51:41 +0800

    Update interface.py
    Update

--- interface.py
+++ interface.py
@@ -6,20 +6,23 @@ sg.theme("DarkTeal6")  # Add a touch of color
 query_column = [
     [
         sg.Text("Query 1:"),
-        sg.Multiline(size=(60, 10), key="-QUERY1-"),
+        sg.Multiline(size=(60, 10), key="-QUERY1-", enable_events=True,  background_color='white'),
     ],
     [
         sg.Text("Query 2:"),
-        sg.Multiline(size=(60, 10), key="-QUERY2-"),
+        sg.Multiline(size=(60, 10), key="-QUERY2-", enable_events=True, background_color='white'),
     ],
 ]
 queryplan_column = [
     [
-        sg.Multiline(size=(60, 10), key="-QUERYPLAN1-", disabled=True),
+        sg.Multiline(size=(60, 10), key="-QUERYPLAN1-", disabled=True, background_color='white'),
     ],
     [
-        sg.Multiline(size=(60, 10), key="-QUERYPLAN2-", disabled=True),
+        sg.Multiline(size=(60, 10), key="-QUERYPLAN2-", disabled=True, background_color='white'),
     ],
+]
+error_column = [
+    [sg.Text(size=(75, 1), key="-ERROR-", text_color="red")],
 ]
 button_column = [
     [sg.Button("Compare", key="-COMPARE-"), sg.Button("Quit", key="-QUIT-")],
@@ -30,6 +33,7 @@ layout = [
         sg.VSeperator(),
         sg.Column(queryplan_column),
     ],
+    [sg.Column(error_column, vertical_alignment="center", justification="center")],
     [sg.Column(button_column, vertical_alignment="center", justification="center")],
 ]
 
@@ -45,12 +49,35 @@ while True:
     ):  # if user closes window or clicks cancel
         break
     elif event == "-COMPARE-":
-        # Check if the text box is empty
-        if values["-QUERY1-"] == "" or values["-QUERY2-"] == "":
-            sg.popup("Either Query 1 or Query 2 is empty!", title="Error Message")
-            continue
+        error_msg = ""
+        # Clear previous message
+        window["-QUERYPLAN1-"].update("")
+        window["-QUERYPLAN2-"].update("")
 
-        window["-QUERYPLAN1-"].update(f'Query 1: {values["-QUERY1-"]}')
-        window["-QUERYPLAN2-"].update(f'Query 2: {values["-QUERY2-"]}')
+        if not values["-QUERY1-"]:
+            window["-QUERY1-"].update(background_color='pink')
+            error_msg += "Please enter a query in Query 1.\n"
+        else:
+            window["-QUERY1-"].update(background_color='white')
+
+        if not values["-QUERY2-"]:
+            window["-QUERY2-"].update(background_color='pink')
+            error_msg += "Please enter a query in Query 2.\n"
+        else:
+            window["-QUERY2-"].update(background_color='white')
+
+        if error_msg:
+            window["-ERROR-"].update(error_msg)
+        else:
+            window["-QUERYPLAN1-"].update(f'Query 1: {values["-QUERY1-"]}')
+            window["-QUERYPLAN2-"].update(f'Query 2: {values["-QUERY2-"]}')
+
+            # Clear any existing error messages
+            window["-ERROR-"].update("")
+
+    elif event == "-QUERY1-" or event == "-QUERY2-":
+        if values[event]:
+            window[event].update(background_color="white")
+
 
 window.close()
