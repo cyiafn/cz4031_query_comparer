f3ef0b2338011d68622069c9a072571da37fd2eb
soulpower11 <5299979+soulpower11@users.noreply.github.com>
2023-03-27 23:39:17 +0800

    explaination

--- interface.py
+++ interface.py
@@ -8,11 +8,9 @@ os.environ["PATH"] += os.pathsep + "Graphviz/bin"  # Set Graphviz PATH
 
 sg.theme("DarkTeal6")  # Add a touch of color
 
-# Todo: Process the plan tree into a simple tree
-# Add one more text box for explaination
-
 # All the stuff inside your window.
 query_column = [
+    [sg.Text("Query", font=("Helvitica", "16", "bold"))],
     [
         sg.Text("Query 1:"),
         sg.Multiline(
@@ -30,6 +28,7 @@ query_column = [
 queryplan1_image = [[sg.Image(size=(400, 300), key="-QUERYPLAN1IMAGE-")]]
 queryplan2_image = [[sg.Image(size=(400, 300), key="-QUERYPLAN2IMAGE-")]]
 queryplanimage_column = [
+    [sg.Text("Query Plan Visualization", font=("Helvitica", "16", "bold"))],
     [sg.Column(queryplan1_image, scrollable=True, key="-QUERYPLAN1IMAGECOLUMN-")],
     [sg.Column(queryplan2_image, scrollable=True, key="-QUERYPLAN2IMAGECOLUMN-")],
 ]
@@ -44,6 +43,19 @@ queryplan_column = [
         sg.Multiline(
             size=(60, 10), key="-QUERYPLAN2-", disabled=True, background_color="white"
         ),
+    ],
+]
+
+
+explaination_column = [
+    [sg.Text("Explaination", font=("Helvitica", "16", "bold"))],
+    [
+        sg.Multiline(
+            size=(1620, 10),
+            key="-EXPLAINATION-",
+            disabled=True,
+            background_color="white",
+        )
     ],
 ]
 
@@ -63,6 +75,11 @@ layout = [
         sg.VSeperator(),
         sg.Column(queryplanimage_column),
     ],
+    [
+        sg.Column(
+            explaination_column, vertical_alignment="center", justification="center"
+        )
+    ],
     [sg.Column(error_column, vertical_alignment="center", justification="center")],
     [sg.Column(button_column, vertical_alignment="center", justification="center")],
 ]
@@ -80,23 +97,25 @@ def build_tree(node, parent=None):
 def compare_btn(window: sg.Window, event, values):
     error_msg = ""
 
-    if not values["-QUERY1-"]:
+    if not values["-QUERY1-"] and not values["-QUERY2-"]:
         window["-QUERY1-"].update(background_color="pink")
-        error_msg += "Please enter a query in Query 1.\n"
-    else:
-        window["-QUERY1-"].update(background_color="white")
-
-    if not values["-QUERY2-"]:
         window["-QUERY2-"].update(background_color="pink")
-        error_msg += "Please enter a query in Query 2.\n"
-    else:
-        window["-QUERY2-"].update(background_color="white")
+        error_msg = "Please enter a query in Query 1 and Query 2."
+    elif not values["-QUERY1-"]:
+        window["-QUERY1-"].update(background_color="pink")
+        error_msg = "Please enter a query in Query 1."
+    elif not values["-QUERY2-"]:
+        window["-QUERY2-"].update(background_color="pink")
+        error_msg = "Please enter a query in Query 2."
 
     if error_msg:
-        window["-ERROR-"].update(error_msg, background_color="white")
+        window["-ERROR-"].update(error_msg)
     else:
-        q_plan_1 = query(values["-QUERY1-"])
-        q_plan_2 = query(values["-QUERY2-"])
+        query_1 = window["-QUERY1-"].get()
+        query_2 = window["-QUERY2-"].get()
+
+        q_plan_1 = query(query_1)
+        q_plan_2 = query(query_2)
 
         tree_1 = build_tree(q_plan_1["Plan"])
         tree_2 = build_tree(q_plan_2["Plan"])
@@ -104,31 +123,28 @@ def compare_btn(window: sg.Window, event, values):
         # Export tree to PNG file using UniqueDotExporter
         filename_1 = "tree_1.png"
         filename_2 = "tree_2.png"
+
         dot_exporter = UniqueDotExporter(tree_1)
         dot_exporter.to_picture(filename_1)
         window["-QUERYPLAN1IMAGE-"].update(filename_1)
+
         dot_exporter = UniqueDotExporter(tree_2)
         dot_exporter.to_picture(filename_2)
         window["-QUERYPLAN2IMAGE-"].update(filename_2)
+
+        start = query_1.find("select")
+        end = start + len("select")
+        window["-QUERY1-"].Widget.tag_add("highlight", f"1.{start}", f"1.{end}")
+
+        start = query_2.find("n_name")
+        end = start + len("n_name")
+        window["-QUERY2-"].Widget.tag_add("highlight", f"1.{start}", f"1.{end}")
 
         # Refresh the update
         window.refresh()
         # Update for scroll area of Column element
         window["-QUERYPLAN1IMAGECOLUMN-"].contents_changed()
         window["-QUERYPLAN2IMAGECOLUMN-"].contents_changed()
-
-        text = window["-QUERY1-"].get()
-        start = text.find("select")
-        end = start + len("select")
-        window["-QUERY1-"].Widget.tag_add("highlight", f"1.{start}", f"1.{end}")
-
-        text = window["-QUERY2-"].get()
-        start = text.find("n_name")
-        end = start + len("n_name")
-        window["-QUERY2-"].Widget.tag_add("highlight", f"1.{start}", f"1.{end}")
-
-        # Clear any existing error messages
-        window["-ERROR-"].update("")
 
 
 def start_ui() -> None:
@@ -137,6 +153,7 @@ def start_ui() -> None:
         "SQL Query Comparer", layout, size=(1620, 820), margins=(26, 26), finalize=True
     )
 
+    # Add tag for highting the diff
     window["-QUERY1-"].Widget.tag_config("highlight", foreground="red")
     window["-QUERY2-"].Widget.tag_config("highlight", foreground="red")
 
@@ -149,65 +166,27 @@ def start_ui() -> None:
         ):  # if user closes window or clicks cancel
             break
         elif event == "-COMPARE-":
+            compare_btn(window, event, values)
+        elif event == "-QUERY1-":
             error_msg = ""
-            # # Clear previous message
-            # window["-QUERYPLAN1-"].update("")
-            # window["-QUERYPLAN2-"].update("")
-
-            if not values["-QUERY1-"]:
-                window["-QUERY1-"].update(background_color="pink")
-                error_msg += "Please enter a query in Query 1.\n"
-            else:
-                window["-QUERY1-"].update(background_color="white")
-
-            if not values["-QUERY2-"]:
-                window["-QUERY2-"].update(background_color="pink")
-                error_msg += "Please enter a query in Query 2.\n"
-            else:
-                window["-QUERY2-"].update(background_color="white")
-
-            if error_msg:
-                window["-ERROR-"].update(error_msg, background_color="white")
-            else:
-                q_plan_1 = query(values["-QUERY1-"])
-                q_plan_2 = query(values["-QUERY2-"])
-
-                tree_1 = build_tree(q_plan_1["Plan"])
-                tree_2 = build_tree(q_plan_2["Plan"])
-
-                # Export tree to PNG file using UniqueDotExporter
-                filename_1 = "tree_1.png"
-                filename_2 = "tree_2.png"
-                dot_exporter = UniqueDotExporter(tree_1)
-                dot_exporter.to_picture(filename_1)
-                window["-QUERYPLAN1IMAGE-"].update(filename_1)
-                dot_exporter = UniqueDotExporter(tree_2)
-                dot_exporter.to_picture(filename_2)
-                window["-QUERYPLAN2IMAGE-"].update(filename_2)
-
-                # Refresh the update
-                window.refresh()
-                # Update for scroll area of Column element
-                window["-QUERYPLAN1IMAGECOLUMN-"].contents_changed()
-                window["-QUERYPLAN2IMAGECOLUMN-"].contents_changed()
-
-                text = window["-QUERY1-"].get()
-                start = text.find("select")
-                end = start + len("select")
-                window["-QUERY1-"].Widget.tag_add("highlight", f"1.{start}", f"1.{end}")
-
-                text = window["-QUERY2-"].get()
-                start = text.find("n_name")
-                end = start + len("n_name")
-                window["-QUERY2-"].Widget.tag_add("highlight", f"1.{start}", f"1.{end}")
-
-                # Clear any existing error messages
-                window["-ERROR-"].update("")
-
-        elif event == "-QUERY1-" or event == "-QUERY2-":
-            if values[event]:
+            if values[event] and not values["-QUERY2-"]:
+                window[event].update(background_color="white")
+                error_msg = "Please enter a query in Query 2."
+            elif values[event] and values["-QUERY2-"]:
                 window[event].update(background_color="white")
 
+            if window["-ERROR-"].get():
+                window["-ERROR-"].update(error_msg)
+        elif event == "-QUERY2-":
+            error_msg = ""
+            if values[event] and not values["-QUERY1-"]:
+                window[event].update(background_color="white")
+                error_msg = "Please enter a query in Query 1."
+            elif values[event] and values["-QUERY1-"]:
+                window[event].update(background_color="white")
+
+            if window["-ERROR-"].get():
+                window["-ERROR-"].update(error_msg)
     window.close()
 
 
