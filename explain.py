d482f05774c343884fc191639ca2c27af9194a10
Jeromelee97 <jeromeleeking1997@gmail.com>
2023-03-31 20:40:59 +0800

    add

--- explain.py
+++ explain.py
@@ -1,15 +1,26 @@
 from project import QueryPlan, QueryPlanJoinNode, query
 import re
 
-diffInQuery = [
-    'Removed: c_custkey = o_custkey',
-    "Modified: AND r_name = 'ASIA'  => AND r_name = 'nonASIA'",
-    'Removed: AND c_acctbal > 10',
-    "Added: AND customer.name LIKE 'cheng'"
-]
+diffInQuery = {
+    'Removed': ["AND r_name = 'ASIA'", 'AND s_acctbal > 20'],
+    'Modified': [["  AND o_orderdate < '1995-01-01'"], ["  AND o_orderdate < '1996-01-01'"], ['  AND s_acctbal > 20'], ['  AND s_acctbal > 10']],
+    'Added': ["AND customer.name LIKE 'cheng'"]
+}
 
 #def explain(diffInQuery, diffInPlan):
 def explain():
+    diff = []
+    for key, value in diffInQuery.items():
+        diff.append(f"{key}: {str(value)}")
+
+    diff = [s.replace("'", "\'") for s in diff]
+
+    for i in range(len(diff)):
+        diff[i] = diff[i].replace("[[", "[").replace("]]", "]")
+        if "[" in diff[i]:
+            diff[i] = diff[i][:-1] + "', " + diff[i][-1]
+        else:
+            diff[i] = diff[i][:-1] + "']" + diff[i][-1]
     explaination = ""
     
     # Define removed/modified/added
@@ -23,7 +34,7 @@ def explain():
     tuple_attr_regex = re.compile(r"([\w\._]+)(?=[\s]*[=<>])")
 
     count = 0
-    for item in diffInQuery:
+    for item in diff:
         if count == 1:
             explaination += "and with "
         else:
@@ -72,19 +83,27 @@ def explain():
             tuple_attrs.append(match)
 
         # Print the resulting lists
-        #print("Comparisons:", comparisons)
+        print("Comparisons:", comparisons)
         count1 = 0
         if status == 'Modified':
-            explaination += "join condition changes from "
+            explaination += "join condition that changes from "
             for i in comparisons:
-                explaination += " ".join(str(j) for j in i) + " "
+                if count1%2 == 0 and count1 != 0:
+                    explaination += "and "
+                    count1 = 0
+                for j in i:
+                    explaination += (str(j)) + " "
                 if count1 == 0:
                     explaination += "to "
-                count1 = 1
+                count1 += 1
         else:
-            explaination += "join condition between "
+            explaination += "join condition such as "
             for i in comparisons:
+                if count1%2 == 1:
+                    explaination += "and "
+                    count1 = 0
                 explaination += " ".join(str(j) for j in i) + " "
+                count1 += 1
 
 
     query_1 = "SELECT * FROM customer C, orders O WHERE C.c_custkey = O.o_custkey"
@@ -96,10 +115,10 @@ def explain():
     q_plan_1_nodes = QueryPlan(q_plan_1["Plan"])
     q_plan_2_nodes = QueryPlan(q_plan_2["Plan"])
 
-    print(q_plan_1_nodes.root.JoinCond)
-    print(q_plan_1_nodes.root.node)
-    print_nodes(q_plan_1_nodes.root)
-    build_tree_diff(q_plan_1_nodes.root)
+    #print(q_plan_1_nodes.root.JoinCond)
+    #print(q_plan_1_nodes.root.node)
+    #print_nodes(q_plan_1_nodes.root)
+    #build_tree_diff(q_plan_1_nodes.root)
 
     #diffInPlan = Selection/Join/Projection
     # queryTwo = QueryPlan.root.getJoinConditions()
@@ -122,7 +141,7 @@ def explain():
 
     explaination += ",the query plan uses sort-merge join instead. \nBecause,"
     explaination = explaination.replace("(", "").replace(")", "").replace("'", "")
-    #print(explaination)
+    print(explaination)
 
 def print_nodes(node):
     if node is None:
